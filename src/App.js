b06b22693c9a006d7993a9d2795c23cfd841b706
Belen Paredes <belen_ub_@hotmail.com>
2021-01-27 09:48:50 +0000

    added support files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Packages from './Packages';
+import  Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Packages APP - Online Store</h1>
+      <Footer/>
       <Packages/> 
     </div>
   );
