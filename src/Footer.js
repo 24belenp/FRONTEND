82a3a3c440898a5fea0e53d6188e02871361f1aa
Belen Paredes <belen_ub_@hotmail.com>
2021-01-27 09:47:14 +0000

    color and footer for fontend

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>FIS.Group:4</h1>Belén Paredes y Yassin Lalj 
+            </div>
+
+        )
+    };
+}
+
+export default Footer;
